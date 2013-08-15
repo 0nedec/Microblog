b3e34b5b3bdd77175190938c8f50d2cc32485b50
Carlos Cedeno <CarlosAntonioCedeno@gmail.com>
2013-08-14 22:43:06 -0400

    Incorporated full text search for all new posts, up to date with tutorial number 10

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from app import db
+from app import app, db
 from hashlib import md5
+import flask.ext.whooshalchemy as whooshalchemy
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -70,6 +71,7 @@ class User(db.Model):
 		return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
 
 class Post(db.Model):
+	__searchable__=['body']
 	id = db.Column(db.Integer, primary_key = True)
 	body = db.Column(db.String(140))
 	timestamp = db.Column(db.DateTime)
@@ -78,4 +80,4 @@ class Post(db.Model):
 	def __repr__(self):
 		return '<Post %r>' % {self.body}
 
-
+whooshalchemy.whoosh_index(app, Post)
