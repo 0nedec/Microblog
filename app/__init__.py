3f45bee38b10e64b427557366b5d3da715d08501
Carlos Cedeno <CarlosAntonioCedeno@gmail.com>
2013-08-28 21:13:18 -0400

    Added Email Capability, MB is now compatible with tutorial XI

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
+from flask.ext.mail import Mail 
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +14,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.loigin_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+mail = Mail(app)
 
 from app import views, models
 
